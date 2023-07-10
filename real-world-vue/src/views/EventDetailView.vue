65927e470e574be8908729e80d9e209338b98cf9
deityzs <kongkonghihi@gmail.com>
2023-07-11 00:48:09 +0700

    Update 3.*.*

--- real-world-vue/src/views/EventDetailView.vue
+++ real-world-vue/src/views/EventDetailView.vue
@@ -1,6 +1,24 @@
 <template>
   <main>
-    <EventDetail :event="event" ></EventDetail>
+    <!-- <EventDetail :event="event" ></EventDetail> -->
+    <div v-if="event">
+      <h1>{{event.title}}</h1>
+      <div id="nav">
+        <router-link :to="{name: 'EventDetails', params: { id }}">
+          Details
+        </router-link>
+
+        <router-link :to="{name: 'EventRegister', params: { id }}">
+          Register
+        </router-link>
+
+        <router-link :to="{name: 'EventEdit', params: { id }}">
+          Edit
+        </router-link>
+      </div>
+      <p>{{event.time}} on {{event.date}} @ {{ event.location }}</p>
+      <p>{{ event.description }}</p>
+    </div>>
   </main>
 </template>
 
